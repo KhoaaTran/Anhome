bcc6f93ab3fa57ee9224b853a9da19a7168404cf
KhoaaTran <tdkhoa73@gmail.com>
2023-08-08 10:29:59 +0700

    update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const mobie_nav = document.querySelector("#mobile-nav");
+
 mobie_nav.addEventListener("click", function (e) {
     const menu_mobie = e.target.closest(".menu-inner");
     const menu_checkbox = menu_mobie.previousElementSibling;
